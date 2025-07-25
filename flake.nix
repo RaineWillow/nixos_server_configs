86871741e3c45ffcb9ca1f4b59da740ce7c22b44
Jade Harley <jade@witchof.space>
2025-07-25 12:35:05 -0400

    Moved server config to its own folder

--- flake.nix
+++ flake.nix
@@ -6,14 +6,19 @@
     nixosConfigurations.brain-ghost = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
+        # These are in the flake so we dont have to bother with passing the nixpkgs
+        # flake to configuration.nxi
         {
+          # Sets system revision from git revision of this repo
           system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
+          # Sets up nix shell and nix-shell type refs
           nix = {
             registry.nixpkgs.flake = nixpkgs;
             nixPath = [ "nixpkgs=${nixpkgs}" ];
           };
         }
-        ./configuration.nix
+        # Import the actual system config
+        ./system/brain-ghost/configuration.nix
       ];
     };
   };
