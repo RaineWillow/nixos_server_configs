9ea02c08726c4aba51d9c6f1e955f8e36dd3381e
Jade Harley <jade@witchof.space>
2025-12-05 21:08:04 -0500

    meow

--- system/autoresponder/minecraft.nix
+++ system/autoresponder/minecraft.nix
@@ -6,11 +6,11 @@
     allowedTCPPorts = [ 25565 ];
     allowedUDPPorts = [ 25565 ];
   };
-  users.users.foxcraft = {
+  users.users.foxcraft = let anna_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAMEP1g9ZzmvP1wMn8xULpW6RsAy0GsoJP9/0nowu9mD annaa@nixos-coronation"; in {
     isNormalUser = true; # Definitely not true but okay
     hashedPassword = "!";
     openssh.authorizedKeys.keys = config.users.users.jade.openssh.authorizedKeys.keys ++
-      config.users.users.willow.openssh.authorizedKeys.keys;
+      config.users.users.willow.openssh.authorizedKeys.keys ++ [ anna_key ];
     linger = true;
   };
 }
