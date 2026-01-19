ed23f7c6aaced74500bbbdf778069bf6bb26c425
Jade Harley <jade@witchof.space>
2026-01-19 17:11:49 -0500

    upd8

--- system/brain-ghost/ai.nix
+++ system/brain-ghost/ai.nix
@@ -4,7 +4,7 @@
     # Use the tailscale ip
     host = "100.81.155.79";
     environment = {
-      OLLAMA_API_BASE_URL = "http://100.127.78.38:11434";
+      OLLAMA_API_BASE_URL = "http://autoresponder.halfmoon-mixolydian.ts.net:11434";
     };
   };
 }
