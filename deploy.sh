34ed36f542fbe5de53c919de406add7661cb9d25
Jade Harley <jade@witchof.space>
2026-01-19 17:10:34 -0500

    update

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-HOST=brain-ghost
 nixos-rebuild switch --flake ".#${HOST}" --target-host "${HOST}" --build-host "${HOST}" --sudo --keep-going --show-trace 
--- system/autoresponder/ai.nix
+++ system/autoresponder/ai.nix
@@ -1,6 +1,7 @@
-{ ... }: {
+{ pkgs, ... }: {
   services.ollama = {
     enable = true;
+    package = pkgs.ollama-cuda;
     openFirewall = true;
     loadModels = [
       "qwen3-vl:32b"
