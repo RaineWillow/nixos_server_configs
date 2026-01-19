a1a863fade05994bd8608c890ecdb4008816b23f
Jade Harley <jade@witchof.space>
2026-01-19 17:30:02 -0500

    Correct cuda version

--- system/autoresponder/ai.nix
+++ system/autoresponder/ai.nix
@@ -1,7 +1,9 @@
 { pkgs, ... }: {
   services.ollama = {
     enable = true;
-    package = pkgs.ollama-cuda;
+    package = pkgs.ollama-cuda.override {
+      cudaPackages = pkgs.cudaPackages_12_2;
+    };
     openFirewall = true;
     loadModels = [
       "qwen3-vl:32b"
