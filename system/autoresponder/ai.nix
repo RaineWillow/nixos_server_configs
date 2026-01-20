2f5caa6cc013cde79473880f32b8da50f5d6c3dc
Jade Harley <jade@witchof.space>
2026-01-19 19:02:19 -0500

    update

--- system/autoresponder/ai.nix
+++ system/autoresponder/ai.nix
@@ -1,10 +1,11 @@
-{ pkgs, ... }: {
+{ config, pkgs, ... }: {
   services.ollama = {
     enable = true;
     package = pkgs.ollama-cuda;
-    openFirewall = true;
+    openFirewall = false;
     loadModels = [
       "qwen3-vl:32b"
     ];
   };
+  networking.firewall.interfaces.tailscale0.allowedTCPPorts = [ config.services.ollama.port ];
 }
